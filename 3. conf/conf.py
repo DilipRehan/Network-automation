3cdb1b079bf9bb64a82fb509f15ffd075fae4f36
Dilip Rehan <dilip.rehan95@gmail.com>
2026-01-19 21:38:18 +0000

    Update conf.py

--- 3. conf/conf.py
+++ 3. conf/conf.py
@@ -1,5 +1,6 @@
 from netmiko import ConnectHandler
 import yaml
+from rich import print as rprint
 
 with open ("device.yaml","r") as target_file:
     device = yaml.safe_load(target_file)
@@ -12,4 +13,11 @@ with open ("device.yaml","r") as target_file:
     with ConnectHandler(**dev) as net_connect:
         output = net_connect.send_command("show running-config ")
         print(output)
+
+    with ConnectHandler(**dev) as net_connect2:
+        output2 = net_connect2.send_command("show snmp community",use_textfsm=True)
+        rprint(output2)
+        rprint(output2[1]["name"]) # Accessing the 'name' field from the second entry
+        if output2[0]["index"] == "cisco0":
+            rprint("This is a Cisco Device")
 
