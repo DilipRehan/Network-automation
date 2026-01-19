10b9958addef21c4d3e81a66c7c094db7f68c2d8
Dilip Rehan <dilip.rehan95@gmail.com>
2026-01-19 19:57:43 +0000

    Update conf.py

--- 3. conf/conf.py
+++ 3. conf/conf.py
@@ -4,8 +4,12 @@ import yaml
 with open ("device.yaml","r") as target_file:
     device = yaml.safe_load(target_file)
 
+
+    #Access to the device dictionary inside the yaml file
     dev = device["device"]
 
+    # Establishing connection to the device
     with ConnectHandler(**dev) as net_connect:
-        output = net_connect.send_command("show running-config | include hostname")
+        output = net_connect.send_command("show running-config ")
+        print(output)
 
