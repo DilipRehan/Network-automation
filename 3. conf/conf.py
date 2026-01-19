f5715cc6b541c69493c75402db3b2c60f853b1f5
Dilip Rehan <dilip.rehan95@gmail.com>
2026-01-19 19:51:18 +0000

    2.0

--- 3. conf/conf.py
+++ 3. conf/conf.py
@@ -1,1 +1,11 @@
 from netmiko import ConnectHandler
+import yaml
+
+with open ("device.yaml","r") as target_file:
+    device = yaml.safe_load(target_file)
+
+    dev = device["device"]
+
+    with ConnectHandler(**dev) as net_connect:
+        output = net_connect.send_command("show running-config | include hostname")
+
