33e5c0cb5bb57b6e3d95af456a454d953df1c97c
Dilip Rehan <dilip.rehan95@gmail.com>
2026-02-01 07:28:53 +0000

    1

--- 4. Netconf/Netconf.py
+++ 4. Netconf/Netconf.py
@@ -1,6 +1,7 @@
 from ncclient import manager #manager object to connec to the device
 from xml.dom import minidom #minidom to pretty print the xml output
 
+#device connection details (im using cisco devnet sandbox device)
 my_device ={
     "host": "10.10.20.48",
     "port": "830",
@@ -9,7 +10,7 @@ my_device ={
     "hostkey_verify": False
 }
 
-
+#define subtree filter to get all interface information
 subtree_filter = """
 <interfaces xmlns="urn:ietf:params:xml:ns:yang:ietf-interfaces">
 <interface>
@@ -19,7 +20,7 @@ subtree_filter = """
 </interfaces> 
 """
 
+#connect to the device and get the running configuration with the defined subtree filter
 with manager.connect(**my_device) as target: 
     result = target.get_config(source="running", filter=("subtree",subtree_filter)).xml
-    
-    print(minidom.parseString(result).toprettyxml())
+    print(minidom.parseString(result).toprettyxml()) #pretty print the xml output
